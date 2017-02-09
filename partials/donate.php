fd14052fc6fece71f1182fdf4a675715e41882ed
Liam Stewart <lstewart2397@gmail.com>
2017-02-09 10:58:24 -0500

    donate page

--- partials/donate.php
+++ partials/donate.php
@@ -9,13 +9,36 @@
 		</div>
 	</div>
 	<div class="row" id="pageContent">
-		<div class="col-xs-12 col-md-12 donate-main text-center"><br><br><br>
-			<div class="col-md-8 col-md-offset-2 donate-text">
-				<p>This section includes pictures of Chantry Island and the area of Lake Huron surrounding the Island. It also includes pictures of the lighthouse and keeper's cottage inside and out, as well as photos of some of the birds and flowers native to the island.</p>
-				<p>If you have photos of Chantry Island that you would like to share with us, connect with our <a target="_blank" href="https://www.facebook.com/MarineHeritageSociety">Facebook</a> page.</p></div>
+		<div class="col-xs-12 col-md-12 donate-main"><br><br><br>
+			<div class="col-md-5 col-md-offset-2 donate-text">
+				<h2>why should you donate?</h2>
+				<h3>Since 1997 we have had a tremendous amount of support by donations.</h3>
+				<br>
+				<p>We are currently engaged in Fund Raising to support our New Boat, Dock Improvements, and Marine Heritage Projects. If you would like to send a cheque or money order, please send to our mailing address.</p>
+				<p>Please specify your Donation Preference:<br>
+				Chantry Island and/or New Tour Boat </p>
+				<p>We will send a tax receipt for all donations $20 and more. </p>
+									<br><br>
+			<br>
 
-				<br><br>
-				<br><br>
+<a id="btnred" class="footer-item book-now" href="#">Donate now</a>
+
+			<br>			<br><br>
+			<br><br>			<br><br>
+			<br><br>
+			</div>
+			<div class="col-md-3 col-md-offset-2 text-center">
+			<div class="donate-sidebar">
+				<br>
+				<h4>Mailing address</h4>
+				<p>
+				Marine Heritage Society<br>
+Box 421<br>
+Southampton, Ontario<br>
+Canada, N0H 2L0<br>
+				</p>
+			</div>
 			</div>
 		</div>
-	</section>
+	</div>
+</section>
