b6cbac190836744bb78b325db174754486c15305
Jillian Matthies <j_matthies2@fanshaweonline.ca>
2017-03-14 21:05:52 -0400

    maps start

--- js/main.js
+++ js/main.js
@@ -1,45 +1,107 @@
 (function() {
-	"use strict";
-	console.log("HELLO WORLD!")
+	var map = new google.maps.Map(document.querySelector('.map-wrapper')),
+	lighthouseIcon = { 
+		url: 'images/lighthouse.png',
+		//scaledSize: new google.maps.Size(25, 25)
+	},
+	geocoder = new google.maps.Geocoder(), 
+	routeBut = document.querySelector('.geocode'),
 
-// JS is enabled! Switch links
-	var logo = document.querySelector(".navbar-brand");
-	var menuHome = document.querySelector("#menuHome");
-	var menuAbout = document.querySelector("#menuAbout");
-	var menuBook = document.querySelector("#menuBook");
-	var menuGallery = document.querySelector("#menuGallery");
-	var menuDonate = document.querySelector("#menuDonate");
-	var menuContact = document.querySelector("#menuContact");
+	directionsService = new google.maps.DirectionsService(),
+	directionsDisplay, 
+	locations = [],
+	marker;
+
+	//console.log(lighthouseIcon);
+
+	function initMap(position) {
+
+locations[0] = {lat: 44.499932, lng: -81.373540};
+directionsDisplay = new google.maps.DirectionsRenderer();
+directionsDisplay.setMap(map);
+directionsDisplay.setPanel(document.querySelector('.directionsPanel'));	
 	
-	logo.href = "#/";
-	menuHome.href = "#/";
-	menuAbout.href = "#/about";
-	menuBook.href = "#/book";
-	menuGallery.href = "#/gallery";
-	menuDonate.href = "#/donate";
-	menuContact.href = "#/contact";
-// end js enabled link switching
+map.setCenter({lat: 44.499932 /*position.coords.latitude*/, lng: -81.373540 /*position.coords.longitude*/ });
+
+map.setZoom(17); 
+
+		marker = new google.maps.Marker({
+			//position: {lat: 42.983233, lng: -81.250688},
+			position: {lat: 44.499932 /*position.coords.latitude*/, lng: -81.373540 /*position.coords.longitude*/},
+			map: map,
+			animation: google.maps.Animation.DROP,
+			icon: lighthouseIcon,  
+			title: "Chantry Island Tours"
+		});	
+
+/*var panorama = new google.maps.StreetViewPanorama(
+            document.querySelector('.streetView'), {
+              position: {lat: 44.499932, lng: -81.373540},
+              pov: {
+                heading: 34,
+                pitch: 10
+              }
+            }); 
+map.setStreetView(panorama);*/		 
 
 
-	
+	}
+
+function mapAddress() {
+	var addressInput = document.querySelector('.address').value;
+	geocoder.geocode( {'address' : addressInput}, function(results, status) {
+		if(status === google.maps.GeocoderStatus.OK) {
+			locations[1] = {lat:results[0].geometry.location.lat(), lng:results[0].geometry.location.lng()};
+
+			map.setCenter(results[0].geometry.location); 
+			if(marker) {
+				marker.setMap(null);
+
+				marker = new google.maps.Marker ({
+					map: map,
+					position: results[0].geometry.location 
+
+				});
+			}
+
+			directions(results[0].geometry.location);
+		} else {
+			console.log("geocoder was not successful");
+		}
+
+	});
+}
+
+function directions(codedLoc) {
+	var request = {
+		origin: locations[1],
+		destination: locations[0],
+		travelMode: 'DRIVING'
+	}; 
+
+	directionsService.route(request, function(response, status){
+		if(status === 'OK') {
+			directionsDisplay.setDirections(response);
+		}
+	})
+}
 
 
 
-            $.getJSON('includes/ajaxQuery.php', { image:this.id }, function(data){
-            console.log(data);
-            // $('.modelName').text(data.modelName);
-            // $('.priceInfo').text(data.pricing);
-            // $('.modelDetails').text(data.modelDetails);
-            });
+	if(navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(initMap, handleError);
+	} else {
+		//give some kind of error message to user 
+		console.log("your browser does not support geolocation.");
+	}
+
+	function handleError(e) {
+		console.log(e);
+	}
 
 
 
-	
-	//Variables
 
-	//Functions
-
-	//Listeners
-
+routeBut.addEventListener("click", mapAddress, false);
 
 })();
