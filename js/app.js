da95b9f482dd095d75796822c60f8bd5f9ecc82e
Jillian Matthies <j_matthies2@fanshaweonline.ca>
2017-03-15 19:27:12 -0400

    updated map

--- js/app.js
+++ js/app.js
@@ -93,108 +93,116 @@ app.controller('ContactCtrl', [function() {
 
         // MAP
         var map = new google.maps.Map(document.querySelector('.map-wrapper')),
-            lighthouseIcon = {
-                url: 'img/lighthouse.png',
-                //scaledSize: new google.maps.Size(25, 25)
-            },
-            geocoder = new google.maps.Geocoder(),
-            routeBut = document.querySelector('.geocode'),
+    lighthouseIcon = { 
+        url: 'img/lighthouse.png',
+        //scaledSize: new google.maps.Size(25, 25)
+    },
+    geocoder = new google.maps.Geocoder(), 
+    routeBut = document.querySelector('.geocode'),
 
-            directionsService = new google.maps.DirectionsService(),
-            directionsDisplay,
-            locations = [],
-            marker;
+    directionsService = new google.maps.DirectionsService(),
+    directionsDisplay, 
+    locations = [],
+    streetBut = document.querySelector('.streetView'),
+    streetView,
+    marker;
 
-        //console.log(lighthouseIcon);
+    //console.log(lighthouseIcon);
 
-        function initMap(position) {
+    function initMap(position) {
 
-            locations[0] = { lat: 44.499932, lng: -81.373540 };
-            directionsDisplay = new google.maps.DirectionsRenderer();
-            directionsDisplay.setMap(map);
-            directionsDisplay.setPanel(document.querySelector('.directionsPanel'));
+locations[0] = {lat: 44.499932, lng: -81.373540};
+directionsDisplay = new google.maps.DirectionsRenderer();
+directionsDisplay.setMap(map);
+directionsDisplay.setPanel(document.querySelector('.directionsPanel')); 
+    
+map.setCenter({lat: 44.499932 /*position.coords.latitude*/, lng: -81.373540 /*position.coords.longitude*/ });
 
-            map.setCenter({ lat: 44.499932 /*position.coords.latitude*/ , lng: -81.373540 /*position.coords.longitude*/ });
+map.setZoom(17); 
 
-            map.setZoom(17);
+        marker = new google.maps.Marker({
+            //position: {lat: 42.983233, lng: -81.250688},
+            position: {lat: 44.499932 /*position.coords.latitude*/, lng: -81.373540 /*position.coords.longitude*/},
+            map: map,
+            animation: google.maps.Animation.DROP,
+            icon: lighthouseIcon,  
+            title: "Chantry Island Tours"
+        }); 
+     
+ streetView = map.getStreetView();
+ streetView.setPosition({lat: 44.499932, lng: -81.373540});
+ streetView.setPov(({
+          heading: 85,
+          pitch: 0
+        }));
 
-            marker = new google.maps.Marker({
-                //position: {lat: 42.983233, lng: -81.250688},
-                position: { lat: 44.499932 /*position.coords.latitude*/ , lng: -81.373540 /*position.coords.longitude*/ },
-                map: map,
-                animation: google.maps.Animation.DROP,
-                icon: lighthouseIcon,
-                title: "Chantry Island Tours"
-            });
+    }
 
-            /*var panorama = new google.maps.StreetViewPanorama(
-                        document.querySelector('.streetView'), {
-                          position: {lat: 44.499932, lng: -81.373540},
-                          pov: {
-                            heading: 34,
-                            pitch: 10
-                          }
-                        }); 
-            map.setStreetView(panorama);*/
+function mapAddress() {
+    var addressInput = document.querySelector('.address').value;
+    geocoder.geocode( {'address' : addressInput}, function(results, status) {
+        if(status === google.maps.GeocoderStatus.OK) {
+            locations[1] = {lat:results[0].geometry.location.lat(), lng:results[0].geometry.location.lng()};
 
+            map.setCenter(results[0].geometry.location); 
+            if(marker) {
+                marker.setMap(null);
 
-        }
+                marker = new google.maps.Marker ({
+                    map: map,
+                    position: results[0].geometry.location 
 
-        function mapAddress() {
-            var addressInput = document.querySelector('.address').value;
-            geocoder.geocode({ 'address': addressInput }, function(results, status) {
-                if (status === google.maps.GeocoderStatus.OK) {
-                    locations[1] = { lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng() };
+                });
+            }
 
-                    map.setCenter(results[0].geometry.location);
-                    if (marker) {
-                        marker.setMap(null);
-
-                        marker = new google.maps.Marker({
-                            map: map,
-                            position: results[0].geometry.location
-
-                        });
-                    }
-
-                    directions(results[0].geometry.location);
-                } else {
-                    console.log("geocoder was not successful");
-                }
-
-            });
-        }
-
-        function directions(codedLoc) {
-            var request = {
-                origin: locations[1],
-                destination: locations[0],
-                travelMode: 'DRIVING'
-            };
-
-            directionsService.route(request, function(response, status) {
-                if (status === 'OK') {
-                    directionsDisplay.setDirections(response);
-                }
-            })
-        }
-
-
-
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(initMap, handleError);
+            directions(results[0].geometry.location);
         } else {
-            //give some kind of error message to user 
-            console.log("your browser does not support geolocation.");
+            console.log("geocoder was not successful");
         }
 
-        function handleError(e) {
-            console.log(e);
+    });
+}
+
+function directions(codedLoc) {
+    var request = {
+        origin: locations[1],
+        destination: locations[0],
+        travelMode: 'DRIVING'
+    }; 
+
+    directionsService.route(request, function(response, status){
+        if(status === 'OK') {
+            directionsDisplay.setDirections(response);
         }
+    })
+}
 
 
 
-        routeBut.addEventListener("click", mapAddress, false);
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(initMap, handleError);
+    } else {
+        //give some kind of error message to user 
+        console.log("your browser does not support geolocation.");
+    }
+
+    function handleError(e) {
+        console.log(e);
+    }
+
+function showStreet() {
+    var toggle = streetView.getVisible();
+        if (toggle == false) {
+          streetView.setVisible(true);
+        } else {
+          streetView.setVisible(false);
+        }
+}
+
+
+routeBut.addEventListener("click", mapAddress, false);
+streetBut.addEventListener("click", showStreet, false);
+      
         // END MAP
 
 
